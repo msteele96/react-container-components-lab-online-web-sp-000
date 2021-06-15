133a3e4e01f4a88b19c16e271220017ee9a77f64
msteele96 <marcsteele2014@gmail.com>
2021-06-14 20:07:48 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,9 +1,19 @@
 // Code MovieReviews Here
 import React from 'react'
 
-const MovieReviews = (props) => <div className="review-list">{props.reviews.map((review, id) => <Review key={id} review={review}/>)}</div>
+const MovieReviews = (props) => {
+    return <div className="review-list">
+        {props.reviews.map(review => Review(review))}
+    </div>
+}
 
-const Review = (props) => <div className="review"><h1><a href={props.review.link.url}>{props.review.display_title}</a></h1><h2>{props.review.mpaa_rating}</h2><img src={props.review.multimedia.src} alt={props.review.display_title}></img></div>
+const Review = (props) => {
+    return <div className="review" key={props.display_title}>
+        <h1><a href={props.link.url}>{props.display_title}</a></h1>
+        <h2>{props.mpaa_rating}</h2>
+        <img src={props.multimedia.src} alt={props.display_title}></img>
+    </div>
+}
 
 export default MovieReviews
 
